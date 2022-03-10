569daf637feaf56650dcec46f8b0b3c49f27be43
nmbarkade <nmbarkade@gmail.com>
2022-03-10 16:50:21 +0530

    for begin

---
+++ welcome.py
@@ -0,0 +1,8 @@
+print('welcome to python programming language')
+//following program find for given no is even or odd
+num = int (input ("Enter any number to test whether
+if (num % 2) == 0:
+              print ("The number is even")
+else
+	print ("The number is odd") // odd statement
+print ("Program completed")
